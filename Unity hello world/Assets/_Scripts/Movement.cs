f160cf137049ceaeab9d6fa67c1300adb2d94ddb
JohnRay <raschehan@gmail.com>
2018-05-30 21:15:39 +0300

    1
    1

--- Unity hello world/Assets/_Scripts/Movement.cs
+++ Unity hello world/Assets/_Scripts/Movement.cs
@@ -6,6 +6,10 @@ public class Movement : MonoBehaviour {
 
 	[SerializeField] private float angularSpeed = 180;
 	[SerializeField] private float speed = 10;
+	[SerializeField] private float jumpSpeed = 30;
+
+	private float verticalSpeed;
+	private bool isJumping;
 
 	private void Update ()
 	{
@@ -13,13 +17,21 @@ public class Movement : MonoBehaviour {
 		float y = Input.GetAxisRaw ("Third");
 		float z = Input.GetAxisRaw ("Vertical");
 
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			//verticalSpeed = jumpSpeed;
+			GetComponent<Rigidbody>().AddForce(Vector3.up * jumpSpeed);
+			//isJumping = true;
+		}
+
 		Quaternion deltaRotation = Quaternion.Euler (new Vector3 (0f , y * angularSpeed * Time.deltaTime, 0f));
 		transform.rotation *= deltaRotation;
 
-		Vector3 deltaPosition =  (transform.forward * z + transform.right * x) * speed * Time.deltaTime;
+		Vector3 deltaPosition =  ((transform.forward * z + transform.right * x) * speed) * Time.deltaTime;
 
 		if (deltaPosition.magnitude > 1) deltaPosition.Normalize();
 
 		transform.position += deltaPosition;
+
+		//verticalSpeed -= gravity * Time.deltaTime;
 	}
 }
