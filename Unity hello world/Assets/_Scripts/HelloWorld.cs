ddf848b48f10ab72f58c680d104e8d7247dbd046
JohnRay <raschehan@gmail.com>
2018-05-30 20:29:28 +0300

    Initial commit

---
+++ Unity hello world/Assets/_Scripts/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello world!1");
+	}
+
+	public float speed = 1;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + new Vector3(speed * Mathf.Cos(Time.time), speed * Mathf.Sin(Time.time), 0f) * Time.deltaTime;
+		transform.rotation = transform.rotation * Quaternion.Euler (new Vector3(80, 80, 80) * Time.deltaTime);
+		transform.localScale = transform.localScale + new Vector3 (0.5f, 0.5f, 0.5f) * Time.deltaTime * Mathf.Sin(Time.time);
+	}
+}
---
+++ Unity hello world/Assets/_Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	[SerializeField] private float angularSpeed = 180;
+	[SerializeField] private float speed = 10;
+
+	private void Update ()
+	{
+		float x = Input.GetAxisRaw ("Horizontal");
+		float y = Input.GetAxisRaw ("Third");
+		float z = Input.GetAxisRaw ("Vertical");
+
+		Quaternion deltaRotation = Quaternion.Euler (new Vector3 (0f , y * angularSpeed * Time.deltaTime, 0f));
+		transform.rotation *= deltaRotation;
+
+		Vector3 deltaPosition =  (transform.forward * z + transform.right * x) * speed * Time.deltaTime;
+
+		if (deltaPosition.magnitude > 1) deltaPosition.Normalize();
+
+		transform.position += deltaPosition;
+	}
+}
