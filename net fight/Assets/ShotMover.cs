267eec0ab0240f9b0a7e5d80886f3a6a4b516f44
JohnRay <raschehan@gmail.com>
2018-08-13 17:09:26 +0300

    13082018

---
+++ net fight/Assets/ShotMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotMover : MonoBehaviour {
+
+    public Vector3 direction;
+
+    private Vector3 dir;
+    private float myTime = 0f;
+    private GameObject me;
+
+	// Use this for initialization
+	void Start () {
+        dir = direction;
+        me = GetComponent<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(dir * Time.deltaTime);
+        myTime += Time.deltaTime;
+        if (myTime > 3f)
+            Destroy(me);
+	}
+}
---
+++ net fight/Assets/moveball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveball : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float f = Input.GetAxisRaw("Vertical") * 300;
+        transform.Translate(0, 0, 0);
+	}
+}
